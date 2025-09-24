23356dd3cf983586ef7b6754d096766f678b3f8d
danyg <daniel.goberitz@treatwell.com>
2025-09-24 14:09:39 +0000

    fixes

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,41 +1,49 @@
 import "reflect-metadata";
-import puppeteer from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
 import config from "../config/config";
 import { AppDataSource } from "../db/data-source";
-import { WatchedProduct, PriceHistory } from "../db/entities";
+import * as DB from "../db/entities";
 import { notifyPriceDrop } from "../notifier/notifyPriceDrop";
+
+import { WatchedProduct } from "../types";
 
 export async function scrapeAndStore() {
   await AppDataSource.initialize();
-  const browser = await puppeteer.launch({ headless: true });
+
+  log("root", "Launching browser...");
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--disable-extensions"],
+  });
   const page = await browser.newPage();
+  const getElementValue = getElementValueCreator(page);
 
   for (const product of config.watchedProducts) {
+    log(`scraping|${product.name}`, `navigating to page...`);
     await page.goto(product.url, { waitUntil: "networkidle2" });
-    const priceText = await page.$eval(
-      product.selectorWithPrice,
-      (el: Element) => el.textContent || ""
-    );
+    
+
+    log(`scraping|${product.name}`, `waiting for price...`);
+    await waitForPrice(page, product);
+
+    const priceText = await getElementValue(product.selectorWithPrice);
+    log(`scraping|${product.name}`, `price raw is ${priceText}`);
+
     const currentPrice = parseFloat(
-      priceText.replace(/[^\d.,]/g, "").replace(",", ".")
+      priceText.replace(/[^\d.,]/g, "").replace(",", "")
     );
+    log(`scraping|${product.name}`, `current price is ${currentPrice}`);
+    await persistHistory(product, currentPrice);
 
-    let productEntity = await AppDataSource.manager.findOneBy(WatchedProduct, {
-      url: product.url,
-    });
-    if (!productEntity) {
-      productEntity = AppDataSource.manager.create(WatchedProduct, product);
-      await AppDataSource.manager.save(productEntity);
-    }
-    const history = AppDataSource.manager.create(PriceHistory, {
-      product: productEntity,
-      price: currentPrice,
-    });
-    await AppDataSource.manager.save(history);
-
+    log(
+      `scraping|${product.name}`,
+      `checking against threshold ${product.targetPrice}...`
+    );
     if (currentPrice <= product.targetPrice) {
+      log(`scraping|${product.name}`, `sending notification...`);
       await notifyPriceDrop(product, currentPrice);
     }
+    log(`scraping|${product.name}`, `done!`);
   }
   await browser.close();
 }
@@ -46,3 +54,45 @@ if (require.main === module) {
     process.exit(1);
   });
 }
+
+const log = (context: string, ...args: any[]) => {
+  console.log(`[${new Date().toISOString()}] Scraper<${context}>: `, ...args);
+};
+
+async function persistHistory(product: WatchedProduct, currentPrice: number) {
+  log(`scraping|${product.name}`, `persisting history into db...`);
+  let productEntity = await AppDataSource.manager.findOneBy(DB.WatchedProduct, {
+    url: product.url,
+  });
+  if (!productEntity) {
+    productEntity = AppDataSource.manager.create(DB.WatchedProduct, product);
+    await AppDataSource.manager.save(productEntity);
+  }
+  const history = AppDataSource.manager.create(DB.PriceHistory, {
+    product: productEntity,
+    price: currentPrice,
+  });
+  await AppDataSource.manager.save(history);
+  log(`scraping|${product.name}`, `history persisted into db.`);
+}
+
+async function waitForPrice(page: Page, product: WatchedProduct) {
+  let lastValue = "null";
+  let currentValue = "false";
+  const getElementValue = getElementValueCreator(page);
+  do {
+    lastValue = await getElementValue(product.selectorWithPrice);
+    await wait(250);
+    currentValue = await getElementValue(product.selectorWithPrice);
+  } while (currentValue !== lastValue);
+}
+
+const getElementValueCreator =
+  (page: Page) =>
+  (selector: string): Promise<string> =>
+    page
+      .locator(selector)
+      .map((t) => t.textContent)
+      .wait();
+
+const wait = (t: number) => new Promise((r) => setTimeout(r, t));
